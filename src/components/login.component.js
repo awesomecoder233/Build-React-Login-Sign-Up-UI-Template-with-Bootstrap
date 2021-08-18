69a58ffbbfadb088a9a709c21626e47e8240551e
awesomecoder233 <tommount58@gmail.com>
2021-08-18 08:25:46 +0200

    create a login component

---
+++ src/components/login.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class login extends Component {
+    render() {
+        return (
+            <div>
+                <h3>React Login Component</h3>
+            </div>
+        );
+    }
+}
