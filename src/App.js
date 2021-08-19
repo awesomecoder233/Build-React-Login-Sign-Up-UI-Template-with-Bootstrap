5cb2700afe5271a6a953e6347c7a73b46b892624
awesomecoder233 <tommount58@gmail.com>
2021-08-19 07:13:09 +0200

    add browser router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
