31c42af5072dc0360d81a82961fec8f830959184
awesomecoder233 <tommount58@gmail.com>
2021-08-21 13:48:16 +0200

    creating handlechange&handlesubmit function

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -1,7 +1,63 @@
 import React, { Component } from "react";
 
+
+const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+const validateForm = (errors) => {
+    let valid = true;
+    Object.values(errors).forEach(
+      (val) => val.length > 0 && (valid = false)
+    );
+    return valid;
+}
 export default class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          firstName: null,
+          lastName: null,
+          email: null,
+          password: null,
+          errors: {
+            fullName: '',
+            email: '',
+            password: '',
+          }
+        };
+    }
+    handleChange = (event) => {
+        event.preventDefault();
+        const { name, value } = event.target;
+        let errors = this.state.errors;
+        switch(name) {
+          case 'firstName':
+            errors.firstName = value.length < 5 ? 'First Name must be 5 characters long!' : '';
+            break;
+        case 'lastName':
+            errors.lastName = value.length < 5 ? 'Last Name must be 5 characters long!' : '';
+            break;
+          case 'email':
+            errors.email = validEmailRegex.test(value) ? '' : 'Email is not valid!';
+            break;
+          case 'password':
+            errors.password = value.length < 8 ? 'Password must be characters long!' : '';
+            break;
+          default:
+            break;
+        }
+        this.setState({errors, [name]: value}, ()=>{
+          console.log(errors)
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if(validateForm(this.state.errors)) {
+          console.info('Valid Form')
+        } else {
+          console.error('Invalid Form')
+        }
+    }
     render() {
+        const {errors} = this.state;
         return (
             <form onSubmit={this.handleSubmit} noValidate >
                 <h3>Sign Up</h3>
@@ -16,8 +72,8 @@ export default class SignUp extends Component {
                 <div className="form-group">
                     <label>Last name</label>
                     <input type="text" className="form-control" name="lastName" onChange={this.handleChange} placeholder="Last name" noValidate/>
-                    {errors.lastName.length > 0 &&
-                    <span className='error'>{errors.lastName}</span>}
+                    {/* {errors.lastName.length > 0 &&
+                    <span className='error'>{errors.lastName}</span>} */}
                 </div>
 
                 <div className="form-group">
