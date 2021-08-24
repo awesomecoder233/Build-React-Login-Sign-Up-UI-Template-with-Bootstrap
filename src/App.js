356e43dcd82856ebe4a2f0564b59955b8e5cb450
awesomecoder233 <tommount58@gmail.com>
2021-08-24 10:34:55 +0200

    import slider.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'react-slideshow-image/dist/styles.css'
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./components/login.component";
